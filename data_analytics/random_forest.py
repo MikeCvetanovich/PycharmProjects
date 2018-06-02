8683686a91b4f93e09f91629fa2a7eaaba5d7dd1
Mike Cvetanovich <cvetanovich@gmail.com>
2018-06-02 17:05:09 -0400

    Initial commit

---
+++ data_analytics/random_forest.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import numpy as np
+import sklearn as sk
+import tensorflow as tf
+
+module load anaconda3
+
+python ex_5_tensorFlow5.py
+
+
